39ba0025d7cfdedd0f5f34dbf82cd63dcf65f759
rolandawoda <rolandawoda@gmail.com>
2022-02-10 12:26:43 +0100

    chore: removed node-sass

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import './sass/main.scss';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "./sass/main.scss";
 
 class App extends Component {
   render() {
     return (
-
-      <Router>
-        <Switch>
-        <Route exact path="/" component={Home}/>      
-      </Switch>
-      </Router>
-
-      
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }

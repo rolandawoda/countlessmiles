b4290ef7d2a620bed8bd6fc32123549818b247f9
rolandawoda <rolandawoda@gmail.com>
2022-02-12 05:09:55 +0100

    chore: added github pages support

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./sass/main.scss";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="countlessmiles">
         <Routes>
           <Route exact path="/" element={<Home/>} />
         </Routes>
